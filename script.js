1cb8a9e466f1ee3849e6121890ced831f967ae24
Boltunov Artem <tema2000.28@gmail.com>
2019-06-19 22:09:40 +0300

    Add async method fetch

--- script.js
+++ script.js
@@ -1,55 +1,57 @@
 const searchForm = document.querySelector('#search-form');
 const movie = document.querySelector('#movies');
 
-function apiSearch(e) {
-	e.preventDefault();
+function apiSearch(event) {
+	event.preventDefault();
 	const searchText = document.querySelector('.form-control').value;
-	if (searchText.trim() !== '') {
-		const server = 'https://api.themoviedb.org/3/search/multi?api_key=fc80ddabdf82d0b07ef9f66b40290b39&language=ru&query=' + searchText;
-		requestApi('GET', server);
+	if (searchText.trim() === '') {
+		movie.innerHTML = "Упс, что-то пошло не так...";
+		return;
 	}
+
+	const server = 'https://api.themoviedb.org/3/search/multi?api_key=fc80ddabdf82d0b07ef9f66b40290b39&language=ru&query=' + searchText;
+
+	movie.innerHTML = "Загрузка...";
+
+	fetch(server)
+		.then((value) => {
+			if (value.status !== 200) {
+				return Promise.reject(value.status);
+			}
+			return value.json();
+		})
+		.then((output) => {
+			let inner = '';
+			output.results.forEach(function (item) {
+				let nameItem = item.name || item.title,
+					releaseDate = item.release_date || item.first_air_date || "Неизвестно",
+					imgSrc = item.poster_path ? ("https://image.tmdb.org/t/p/w185" + item.poster_path) : "./assets/unknow.jpg",
+					description = item.overview,
+					rate = item.vote_average,
+					href = "https://www.themoviedb.org/" + item.media_type + "/" + item.id;
+
+				inner += `
+					<div class="movie-item">
+						<a href="${href}" target="_blank" class="movie-href">
+							<img class="poster-film float-letf" src="${imgSrc}" alt=""/>
+							<h4 class="text-left">${nameItem}</h4>
+							<strong>(${releaseDate})</strong>
+							<p>${description}</p>
+							<span class="badge badge-info" style="font-size: large">${rate}</span>
+						</a>
+					</div>`;
+			});
+			if (inner !== '') {
+				movie.innerHTML = inner;
+			} else {
+				movie.innerHTML = 'По вашему запросу ничего не найдено';
+			}
+			document.querySelector('.form-control').value = '';
+		})
+		.catch((reason) => {
+			movie.innerHTML = 'Упс, что-то пошло не так...';
+			console.error(reason);
+		})
 }
 
 searchForm.addEventListener('submit', apiSearch);
-
-function requestApi(method, url) {
-	const request = new XMLHttpRequest();
-	request.open(method, url);
-	request.send();
-
-	request.addEventListener('readystatechange', () => {
-		if (request.readyState !== 4) return;
-
-		if (request.status !== 200) {
-			console.log('error: ' + request.status);
-			return;
-		}
-
-		const output = JSON.parse(request.responseText);
-
-		let inner = '';
-		output.results.forEach(function (item) {
-			let nameItem = item.name || item.title,
-			 releaseDate = item.release_date || item.first_air_date || "Неизвестно",
-			 imgSrc = item.poster_path ? ("https://image.tmdb.org/t/p/w185" + item.poster_path) : "./assets/unknow.jpg",
-			 description = item.overview,
-			 rate = item.vote_average,
-			 href = "https://www.themoviedb.org/" + item.media_type + "/" + item.id;
-
-			inner += '<div class="movie-item">'
-				+ '<a href="' + href + '" target="_blank" class="movie-href">'
-				+ '<img class="poster-film float-letf" src="' + imgSrc + '"  alt=""/>'
-				+ '<h4 class="text-left">' + nameItem + '</h4>'
-				+ '<strong>(' + releaseDate + ')</strong>'
-				+ '<p>' + description + '</p>'
-				+ '<span class="badge badge-info" style="font-size: large">'+ rate +'</span>'
-				+ '</a>'
-				+ '</div>';
-		});
-		if (inner !== '') {
-			movie.innerHTML = inner;
-		}
-		document.querySelector('.form-control').value = '';
-	});
-
-}
